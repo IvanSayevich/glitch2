6663817c287fcd6eebe5174c8c5922d0eb60e7a5
Glitch (pricey-novel) <none>
2018-09-10 19:35:55 +0000

    🍓😎 Checkpoint ./server.js:1422857/34

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-app.get('/api/timestamp/:time', (req, res) => res.json({echo: req.params.time}));
+app.get('/api/timestamp/:time', (req, res) => res.json({echo: new Date(req.params.time)}));
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
